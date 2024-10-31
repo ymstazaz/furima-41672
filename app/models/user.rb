6b045eaead4fa8182aca3c206d6f785e56b15655
ymstazaz <r37a45d@gmail.com>
2024-10-31 17:34:22 +0900

    ユーザー管理機能の実装（テスト前）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: {
+    with: /\A(\p{Hiragana}|\p{Katakana}|[一-龠々])+\z/
+  }
+  validates :first_name, presence: true, format: {
+    with: /\A(\p{Hiragana}|\p{Katakana}|[一-龠々])+\z/
+  }
+  validates :last_name_kana, presence: true, format: {
+    with: /\A(\p{Katakana})+\z/
+  }
+  validates :first_name_kana, presence: true, format: {
+    with: /\A(\p{Katakana})+\z/
+  }
   validates :birth, presence: true
   validates :password, format: {
     with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).+\z/
