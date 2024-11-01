848cfa4f2d5c643c95fa60f34943f274f847436f
ymstazaz <r37a45d@gmail.com>
2024-11-01 19:13:54 +0900

    ヴァリテーションまで済
    次はコントローラー設定から

---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_delivery_id, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :image, presence: true
+end
