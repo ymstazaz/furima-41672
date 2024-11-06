e68e7149bf16bcbe9471f254de2488507163f486
ymstazaz <r37a45d@gmail.com>
2024-11-06 20:42:59 +0900

    ビフォアアクションでまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :find_item, only: [:show, :edit, :update]
+  
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -19,18 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -42,6 +41,10 @@ class ItemsController < ApplicationController
   private
   def item_params
   params.require(:item).permit( :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :user_id, :image )
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 
 end
