8885c66e066c4c175c910bee2ec6aad62f6d7a43
ymstazaz <r37a45d@gmail.com>
2024-11-07 11:53:09 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :find_item, only: [:show, :edit, :update]
-  
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user_id
-      redirect_to root_path
-    end
+    return if current_user.id == @item.user_id
+
+    redirect_to root_path
   end
 
   def update
@@ -37,14 +37,23 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   private
+
   def item_params
-  params.require(:item).permit( :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :user_id, :image )
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
+                                 :scheduled_delivery_id, :price, :user_id, :image)
   end
 
   def find_item
     @item = Item.find(params[:id])
   end
-
 end
