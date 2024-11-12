1f006bb89eaeeecd43d08d5be832f93267f80770
ymstazaz <r37a45d@gmail.com>
2024-11-12 17:02:52 +0900

    payjpの連動開始（秘密鍵導入前）

---
+++ app/javascript/card.js
@@ -0,0 +1,11 @@
+const pay = () => {
+  const form = document.getElementById('charge-form');
+  if (form) { 
+    form.addEventListener("submit", (e) => {
+      console.log("フォーム送信時にイベント発火");
+      e.preventDefault(); 
+    });
+  }
+};
+
+window.addEventListener("turbo:load", pay);
